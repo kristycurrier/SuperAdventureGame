f5ace1606de55d054405c9cd454f16f7dc2386b1
Kristy Currier <kristy.currier@gmail.com>
2018-10-17 16:14:29 -0400

    Super Adventure tutorial with Lession 10 completed

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -13,6 +13,7 @@ namespace Engine
         public string Description { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+        public List<QuestCompletionItem> QuestCompletionItems { get; set; }
         public Item RewardItem { get; set; }
 
         public Quest(int id, string name, string description, 
@@ -23,6 +24,7 @@ namespace Engine
             Description = description;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            QuestCompletionItems = new List<QuestCompletionItem>();
         }
     }
 }
